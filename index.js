292c501d73b89516de917675c2bd1e24114da194
James Cramer <jcramer@deskviz.com>
2018-09-04 16:21:01 -0400

    added bitdb validation

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const slp = require('./lib/slp');
-const utils = require('./lib/utils');
-const bitbox = require('./lib/bitboxnetwork');
+const slp = require('./lib/slp')
+    , utils = require('./lib/utils')
+    , bitdb = require('./lib/bitdbproxy')
+    , bitbox = require('./lib/bitboxnetwork');
 
 module.exports = {
     slp: slp,
     utils: utils,
-    bitbox: bitbox
+    bitbox: bitbox, 
+    bitdb: bitdb
 }
