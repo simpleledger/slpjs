241a9a1d2a9d1403d6058e9f23d596c6fe5a6862
James Cramer <jcramer@deskviz.com>
2018-10-31 09:15:42 -0400

    expose the bitbox-sdk

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const slp = require('./lib/slp')
     , utils = require('./lib/utils')
     , bitdb = require('./lib/bitdbproxy')
-    , bitbox = require('./lib/bitboxnetwork');
+    , bitbox = require('./lib/bitboxnetwork')
+    , bsdk = require('bitbox-sdk');
 
 module.exports = {
     slp: slp,
     utils: utils,
     bitbox: bitbox, 
-    bitdb: bitdb
+    bitdb: bitdb, 
+    bitboxsdk: bsdk 
 }
