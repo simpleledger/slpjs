f6bced4ad195302f9ef61bce8afae5a15dd5a2cf
James Cramer <jcramer@deskviz.com>
2018-11-10 13:05:26 -0500

    fix error in exporting bitbox-sdk

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-'use strict';
+const BITBOXSDK = require('bitbox-sdk/lib/bitbox-sdk').default
+    , BITBOX = new BITBOXSDK()
 
 const slp = require('./lib/slp')
     , utils = require('./lib/utils')
     , bitdb = require('./lib/bitdbproxy')
-    , bitbox = require('./lib/bitboxnetwork')
-    , bsdk = require('bitbox-sdk');
+    , bitbox = require('./lib/bitboxnetwork');
 
 module.exports = {
     slp: slp,
     utils: utils,
     bitbox: bitbox, 
     bitdb: bitdb, 
-    bitboxsdk: bsdk 
+    bitboxsdk: BITBOX 
 }
