d41fa6c22edd1b71436e1937f1f6413232471c74
James S. Cramer <jcramer@simpleledger.io>
2019-01-21 15:23:16 -0500

    unit tests for SLP parser

--- test/proxyvalidation.js
+++ test/proxyvalidation.js
@@ -1,23 +1,37 @@
 const ProxyValidator = require('../lib/proxyvalidation').ProxyValidation;
 const assert = require('assert');
 
-describe('ProxyValidator', function(){
-    describe('validateSlpTransaction()', function(){
-        let proxy = new ProxyValidator();
-        it('returns true for a valid SEND token transaction', async function(){
+describe('ProxyValidator', function() {
+    let proxy = new ProxyValidator();
+    describe('isValidSlpTxid()', function() {
+        it('returns true for a valid SEND token transaction', async function() {
             let tokenTxnId = '2504b5b6a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76deb45';
             let isValid = await proxy.isValidSlpTxid(tokenTxnId);
             assert.equal(isValid, true);
         });
-        it('returns false for an unknown txid', async function(){
+        it('returns false for an unknown txid', async function() {
             let unknownTxnId = '00000006a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76d0000';
             let isValid = await proxy.isValidSlpTxid(unknownTxnId);
             assert.equal(isValid, false);
         });
-        it('returns true for MINT transaction', async function(){
+        it('returns true for MINT transaction', async function() {
             let mintTxnId = '02db2eabb3f1663daeab0648b22c3f8e13f10709cbb038089c4521f519fc0b89';
             let isValid = await proxy.isValidSlpTxid(mintTxnId);
             assert.equal(isValid, true);
+        });
+    });
+    describe('validateTransactions()', function() {
+        it('works for valid SEND token transactions', async function() {
+            this.timeout(5000);
+            let tokenTxnIds = ['2504b5b6a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76deb45', '2504b5b6a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76deb45', '0004b5b6a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76deb45'];
+            let isValid = await proxy.validateTransactions(tokenTxnIds);
+            assert.deepEqual(isValid, ['2504b5b6a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76deb45', '2504b5b6a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76deb45']);
+        });
+        it('works for invalid SEND token transactions', async function() {
+            this.timeout(5000);
+            let tokenTxnIds = ['0004b5b6a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76deb45', '0004b5b6a6ec42b040a71abce1acd71592f7e2a3e33ffa9c415f91a6b76deb45'];
+            let isValid = await proxy.validateTransactions(tokenTxnIds);
+            assert.deepEqual(isValid, []);
         });
     });
 });
--- test/slptokentype1.js
+++ test/slptokentype1.js
@@ -1,25 +1,41 @@
-let SlpTokenType1 = require('../lib/slptokentype1');
+const BITBOXSDK = require('../node_modules/bitbox-sdk/lib/bitbox-sdk').default;
+const BITBOX = new BITBOXSDK();
 const assert = require('assert');
 
-describe('SlpTokenType1', function(){
-    describe('lokadIdHex()', function(){
-        it('returns the proper lokad id.', () => {
-            assert.equal(true, false);
-        });
-    });
-    describe('buildGenesisOpReturn()', function(){
-        it('returns a properly formed GENESIS OP_RETURN.', () => {
-            assert.equal(true, false);
-        });
-    });
-    describe('buildSendOpReturn()', function(){
-        it('returns a properly formed SEND OP_RETURN.', () => {
-            assert.equal(true, false);
-        });
-    });
-    describe('buildMintOpReturn()', function(){
-        it('returns a properly formed MINT OP_RETURN.', () => {
-            assert.equal(true, false);
+const Slp = require('../lib/slp').Slp;
+const scriptUnitTestData = require('../node_modules/slp-unit-test-data/script_tests.json');
+
+// Overall script error codes:
+// * 1 - Basic error that prevents even parsing as a bitcoin script (currently, only for truncated scripts).
+// * 2 - Disallowed opcodes (after OP_RETURN, only opcodes 0x01 - 0x4e may be used).
+// * 3 - Not an SLP message (script is not OP_RETURN, or lacks correct lokad ID). Note in some implementations, the parsers would never be given such non-SLP scripts in the first place. In such implementations, error code 3 tests may be skipped as they are not relevant.
+
+// Reasons relating to the specified limits:
+// * 10 - A push chunk has the wrong size.
+// * 11 - A push chunk has an illegal value.
+// * 12 - A push chunk is missing / there are too few chunks.
+
+// Specific to one particular transaction type:
+
+// * 21 - Too many optional values (currently, only if there are more than 19 token output amounts)
+
+// Other:
+// * 255 - The SLP token_type field is has an unsupported value. Depending on perspective this is not 'invalid' per se -- it simply cannot be parsed since the protocol for that token_type is not known. Like error code 3 these tests may not be applicable to some parsers.
+
+describe('SlpTokenType1', function() {
+    describe('OP_RETURN Parsing Unit Tests', function() {
+        scriptUnitTestData.forEach((test, i)=> {
+            let slp = new Slp(BITBOX);
+            it(test.msg, () => {
+                let script = new Buffer.from(test.script, 'hex');
+                let eCode = test.code;
+                if(eCode) {
+                    assert.throws(function() { slp.parseSlpOutputScript(script) });
+                } else {
+                    let parsedOutput = slp.parseSlpOutputScript(script);
+                    assert(typeof parsedOutput, 'object');
+                }
+            });
         });
     });
 });
