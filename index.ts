e1b3164ced5ace7037a6127b63d4745ef4f92790
James S. Cramer <jcramer@simpleledger.io>
2019-02-08 13:44:51 -0500

    update exports for TS support via `npm install`

--- index.ts
+++ index.ts
@@ -1,13 +1,6 @@
-import { Slp } from './lib/slp';
-import { Utils } from './lib/utils';
-import { BitdbNetwork } from './lib/bitdbnetwork';
-import { JsonRpcProxyValidator } from './lib/jsonrpcvalidator';
-import { LocalValidator } from './lib/localvalidator';
-import { BitboxNetwork } from './lib/bitboxnetwork';
-
-exports.Slp = Slp;
-exports.Utils = Utils;
-exports.BitboxNetwork = BitboxNetwork;
-exports.BitdbNetwork = BitdbNetwork;
-exports.JsonRpcProxyValidator = JsonRpcProxyValidator;
-exports.LocalValidator = LocalValidator;
+export { Slp } from './lib/slp';
+export { Utils } from './lib/utils';
+export { BitdbNetwork } from './lib/bitdbnetwork';
+export { JsonRpcProxyValidator } from './lib/jsonrpcvalidator';
+export { LocalValidator } from './lib/localvalidator';
+export { BitboxNetwork } from './lib/bitboxnetwork';
