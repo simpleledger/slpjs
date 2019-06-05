562979088e864c6d41783dc75465dc6bf0f71652
James Cramer <jcramer@simpleledger.io>
2019-06-05 17:18:56 -0400

    add Buffer as documentUri type

--- index.ts
+++ index.ts
@@ -46,7 +46,7 @@ export interface SlpTransactionDetails {
     timestamp?: string;
     symbol: string;
     name: string;
-    documentUri: string; 
+    documentUri: string|Buffer; 
     documentSha256: Buffer|null;
     decimals: number;
     containsBaton: boolean;
