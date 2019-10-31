d2ceaa914407c1c73366e0dbf0d4480361e3fb7f
James Cramer <jcramer@simpleledger.io>
2019-10-31 09:00:08 -0400

    follow-up prev commit
    forgot global.d.ts file

--- test/global.d.ts
+++ test/global.d.ts
@@ -1,9 +1,12 @@
 export interface SlpValidityUnitTest {
     description: string;
-    when: SlpTestTxn[]
-    should: SlpTestTxn[]
+    when: SlpTestTxn[];
+    should: SlpTestTxn[];
+    allow_inconclusive: boolean;
+    inconclusive_reason: string;
 }
 
 export interface SlpTestTxn {
-    tx: string, valid: boolean
+    tx: string;
+    valid: boolean;
 }
