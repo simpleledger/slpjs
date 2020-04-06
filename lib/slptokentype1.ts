4a4b9cb35a2f65feaad2d577e65bfd7a739402cf
James Cramer <jcramer@simpleledger.io>
2020-04-06 10:26:49 -0400

    update import for mdm

--- lib/slptokentype1.ts
+++ lib/slptokentype1.ts
@@ -1,5 +1,5 @@
 import BigNumber from "bignumber.js";
-import slpmdm from "slp-mdm";
+import { TokenType1 } from "slp-mdm";
 
 export class SlpTokenType1 {
     static get lokadIdHex() { return "534c5000"; }
@@ -14,16 +14,16 @@ export class SlpTokenType1 {
             initialQuantity: BigNumber,
             type= 0x01,
         ) {
-            return slpmdm.TokenType1.genesis(
+            return TokenType1.genesis(
                 ticker || "", name || "", documentUrl || "",
-                documentHashHex || "", decimals, batonVout, initialQuantity);
+                documentHashHex  || "", decimals, batonVout || null, initialQuantity);
     }
 
     public static buildSendOpReturn(tokenIdHex: string, outputQtyArray: BigNumber[], type= 0x01) {
-        return slpmdm.TokenType1.send(tokenIdHex, outputQtyArray);
+        return TokenType1.send(tokenIdHex, outputQtyArray);
     }
 
     public static buildMintOpReturn(tokenIdHex: string, batonVout: number|null, mintQuantity: BigNumber, type= 0x01) {
-        return slpmdm.TokenType1.mint(tokenIdHex, batonVout, mintQuantity);
+        return TokenType1.mint(tokenIdHex, batonVout, mintQuantity);
     }
 }
